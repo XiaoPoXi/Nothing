39753d466cbf8797983260188870f8c80d855ff3
sunxiwei <1223696182@qq.com>
2020-09-08 18:13:21 +0800

    首页代码提交

---
+++ Nothing/Models/UModel.swift
@@ -0,0 +1,23 @@
+//
+//  UModel.swift
+//  Nothing
+//
+//  Created by xiaoxi on 2020/9/8.
+//  Copyright © 2020 xiaoxi. All rights reserved.
+//
+
+import HandyJSON
+
+struct HomeModel: HandyJSON {
+//    var argCon: Int = 0
+    var title: String?
+//    var argValue: Int = 0
+//    var canEdit: Bool = false
+//    var cover: String?
+//    var isLike: Bool = false
+//    var sortId: Int = 0
+//    var sortName: String?
+//    var title: String?
+//    var subTitle: String?
+//    var rankingType: Int = 0
+}
---
+++ Nothing/Views/Home/HomeNormalTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  HomeNormalTableViewCell.swift
+//  Nothing
+//
+//  Created by xiaoxi on 2020/9/8.
+//  Copyright © 2020 xiaoxi. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class HomeNormalTableViewCell: UITableViewCell {
+
+    let width:CGFloat = UIScreen.main.bounds.width
+    var userLabel:UILabel!
+    var birthDayLabel:UILabel!
+    var sexLabel:UILabel!
+    var iconImg:UIImageView!
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        userLabel = UILabel()
+        userLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        userLabel.textAlignment = .left
+        userLabel.textColor = .red
+        userLabel.text = "那么多遗憾那么多期盼你知道啊"
+        userLabel.numberOfLines = 0
+        self.contentView.addSubview(userLabel)
+        
+        userLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(30)
+            make.centerY.equalToSuperview()
+        }
+ 
+        
+    }
+    
+    ///数据填充
+    var model: HomeModel? {
+        didSet {
+            guard let model = model else { return }
+            userLabel.text = model.title
+        }
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
